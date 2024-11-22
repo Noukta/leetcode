7dd8e59a4d8f932a8daaf31ddd531a5866ebade5
Ahmed Noukta <no9ta-ahmed@live.fr>
2024-11-22 22:34:23 +0100

    optimize 1072 solution

--- 1072.js
+++ 1072.js
@@ -3,52 +3,31 @@
  * @return {number}
  */
 const maxEqualRowsAfterFlips = (matrix) => {
-	const numEqualRows = countEqualRows(matrix);
-	const indexMatrix = extractMatrixIndexes(matrix);
-	const maxEqualRows = countMaxDuplication(indexMatrix);
-
-	return numEqualRows > maxEqualRows ? numEqualRows : maxEqualRows;
+	const patterns = convertMatrixToPatterns(matrix);
+	return countMaxOccurrences(patterns);
 };
 
-function countMaxDuplication(indexMatrix) {
-	const stringifiedArrays = indexMatrix
-		.flat(1)
-		.map((arr) => JSON.stringify(arr));
-	const countMap = stringifiedArrays.reduce((acc, curr) => {
+const convertArrayToPattern = (arr) => {
+	const groups = arr.reduce((acc, curr, idx) => {
+		if (idx === 0 || arr[idx - 1] !== curr) {
+			acc.push([]);
+		}
+		acc[acc.length - 1].push(curr);
+		return acc;
+	}, []);
+	return groups.map((group) => "*".repeat(group.length)).join("|");
+};
+
+const convertMatrixToPatterns = (matrix) => {
+	return matrix.map((row) => {
+		return convertArrayToPattern(row);
+	});
+};
+
+const countMaxOccurrences = (arr) => {
+	const countMap = arr.reduce((acc, curr) => {
 		acc.set(curr, (acc.get(curr) || 0) + 1);
 		return acc;
 	}, new Map());
-	console.log(countMap);
 	return Math.max(...Array.from(countMap.values()));
-}
-
-const extractMatrixIndexes = (matrix) => {
-	return matrix.map((row) => {
-		const zeroIndexes = row.reduce(
-			(acc, val, idx) => (val === 0 ? [...acc, idx] : acc),
-			[]
-		);
-		const oneIndexes = row.reduce(
-			(acc, val, idx) => (val === 1 ? [...acc, idx] : acc),
-			[]
-		);
-		return [zeroIndexes, oneIndexes];
-	});
 };
-
-const countEqualRows = (matrix) => {
-	let count = 0;
-	matrix.forEach((row) => {
-		count += row.some((b) => b === 0) && row.some((b) => b === 1) ? 0 : 1;
-	});
-	return count;
-};
-
-// Example usage
-const matrix = [
-	[0, 1, 0],
-	[1, 1, 0],
-	[0, 0, 1],
-];
-
-console.log(maxEqualRowsAfterFlips(matrix));
